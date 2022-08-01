e902128e5cf2b1b9a4bf5bf8df5446a3c49d60cf
blakeahalt <blakeahalt@gmail.com>
2022-08-01 01:19:54 -0700

    2.7Intersection complete

--- chapter02/2.7Intersection.js
+++ chapter02/2.7Intersection.js
@@ -1,4 +1,4 @@
-// var LinkedList = require('./../util/LinkedList');
+
 class Node {
   constructor(val) {
     this.val = val;
@@ -6,113 +6,111 @@ class Node {
   }
 }
 
-var LinkedList = function (val) {
-  this.val = val;
-  this.next = null;
-};
-
+// const LinkedList = function (val) {
+//   this.val = val;
+//   this.next = null;
+// };
 
 var a = new Node(2)
-var b = new Node(5)
-var c = new Node(14)
-var d = new Node(29)
+var b = new Node(4)
+var c = new Node(6)
+var d = new Node(14)
+var e = new Node(29)
+
+var f = new Node(9)
+var g = new Node(12)
+var h = new Node(14)
+var i = new Node(29)
 
 a.next = b
 b.next = c
 c.next = d
-
-var e = new Node(10)
-var f = new Node(14)
-var g = new Node(29)
-
-e.next = f
+d.next = e
 f.next = g
-// g.next = h
+g.next = d
+h.next = e
 
-// var a = new LinkedList(2)
-// var b = new LinkedList(5)
-// var c = new LinkedList(14)
-// var d = new LinkedList(29)
+        // MY SOLUTION WITH ERROR CHECKS
+        // function intersection(head1, head2) {
+        //   let current1 = head1
+        //   // console.log('head1:', current1);
+        //   // console.log('head1.next:', head1.next);
+        //   // console.log('current1.next:', current1.next);
+        //   let current2 = head2
+        //   // console.log('head2:',current2);
 
-// a.next = b
-// b.next = c
-// c.next = d
+        //   if (head1 === null || head2 === null) {
+        //     return null
+        //   }
 
-// var e = new LinkedList(10)
-// var f = new LinkedList(14)
-// var g = new LinkedList(29)
+        //   while (current1 !== current2) {
 
-// e.next = f
-// f.next = g
-// // g.next = h
+        //     current1 = current1.next
+        //     // console.log('c1->c1.next:',current1);
+        //     current2 = current2.next
+        //     // console.log ('c2->c2.next:',current2);
 
-function intersection (head1, head2) {
-  // The null checking code will handle lists with no loops.
+        //     // console.log('c1===c2:',current1);
+        //     if (current1 === null) current1 = head2
+        //     // console.log('should be head2:', current1);
+            
+        //     if (current2 === null) current2 = head1
+        //     if (current1 === current2) {
+        //       return current1
+        //     }
+        //     // console.log('should be head1:', current2);
+        //     // console.log(current1);
+        //     // console.log(current2);
+        //   }
+        //   // console.log(current1);
+        //   return current1
+        // };
+        
+// MY SOLUTION 
+function intersection(head1, head2) {
   let current1 = head1
-  // console.log('head1:', current1);
-  // console.log('head1.next:', head1.next);
-  // console.log('current1.next:', current1.next);
   let current2 = head2
-  // console.log('head2:',current2);
-  
+
   if (head1 === null || head2 === null) {
     return null
   }
 
   while (current1 !== current2) {
-    
-    current1 = current1.next
-    console.log('c1->c1.next:',current1);
-    current2 = current2.next
-    console.log('c2->c2.next:',current2);
 
-    if(current1 === current2) return current1
-    // console.log('c1===c2:',current1);
+    current1 = current1.next
+    current2 = current2.next
+
     if (current1 === null) current1 = head2
-      // console.log('should be head2:', current1);
-    
     if (current2 === null) current2 = head1
-      // console.log('should be head1:', current2);
-    // console.log(current1);
-    // console.log(current2);
+    if (current1 === current2) {
+      return current1
+    }
   }
-  // console.log(current1);
   return current1
 };
 
 
-// console.log(intersection(a,e));
-  // do {
-  //   hare = hare.next
-  //   tortoise = tortoise.next
-  //   if (!hare || !hare.next) return null
-  //   hare = hare.next
-  // } while (hare !== tortoise)
-
-  // tortoise = head
-
-  // while (hare !== tortoise) {
-  //   hare = hare.next
-  //   tortoise = tortoise.next
-  // }
-
-  // return hare
-
+// OTHER SOLUTION
+//   var intersection = function(headA, headB) {
+//     let currA = headA;
+//     let currB = headB;
+//     // console.log(currA);
+//     // console.log(currB);
+    
+//     while (currA !==currB) {
+//         currA = currA === null ? headB : currA.next;
+//         currB = currB === null ? headA : currB.next;
+        
+//         if (currA === currB) {
+//           return currA;
+//         }
+//       }
+      
+//       console.log('currA:',currA);
+//     return currA;
+// };
 
 // OTHER SOLUTION
-// /* 
-// Given the heads of two singly linked-lists that eventually merge, write code that 
-// returns the node at which the two lists intersect. 
-// Example
-// 2
-//  \
-//   5  10
-//    \ /
-//    14
-//     |
-//    29
-// */
-
 // function intersection(headA, headB) {
 //   let currA = headA;
 //   let currB = headB;
@@ -156,68 +154,6 @@ function intersection (head1, head2) {
 //   return currA;
 // }
 
-// // _________ _______  _______ _________   _______  _______  _______  _______  _______
-// // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
-// //    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
-// //    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
-// //    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
-// //    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
-// //    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
-// //    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
-// //                             ____       _
-// //                             |  _ \     | |
-// //                             | |_) | ___| | _____      __
-// //                             |  _ < / _ \ |/ _ \ \ /\ / /
-// //                             | |_) |  __/ | (_) \ V  V /
-// //                             |____/ \___|_|\___/ \_/\_/
-// //                         ______ ______ ______ ______ ______
-// //                         |______|______|______|______|______|
-
-// //                          ______ ______ ______ ______ ______
-// //                         |______|______|______|______|______|
-
-// //                          ______ ______ ______ ______ ______
-// //                         |______|______|______|______|______|
-
-// mocha.setup("bdd");
-// const { assert } = chai;
-
-// describe("Intersection Node", () => {
-//   it("returns intersection correctly", () => {
-//     const oneNode = new ListNode(1);
-//     const twoNode = new ListNode(2);
-//     const threeNode = new ListNode(3);
-
-//     oneNode.next = threeNode;
-//     twoNode.next = threeNode;
-
-//     assert.equal(getIntersectionNode(oneNode, twoNode), threeNode);
-//   });
-//   it("returns null if no intersection", () => {
-//     const l1 = convertArrToLL([0, 1, 2, 1, 3]);
-//     const l2 = convertArrToLL([5]);
-//     assert.equal(getIntersectionNode(l1, l2), null);
-//   });
-// });
-
-// mocha.run();
-
-/* TEST */
-// A -> B -> C -> D -> E -> C
-
-// var a = new LinkedList(A);
-// var b = new LinkedList(B);
-// var c = new LinkedList(C);
-// var d = new LinkedList(D);
-// var e = new LinkedList(E);
-// var f = new LinkedList(C);
-
-// a.next = b;
-// b.next = c;
-// c.next = d;
-// d.next = e;
-// e.next = f;
-// f.next = c;
 const print = (node) => {
   if (node === null) return;
   console.log(node.val)
@@ -231,40 +167,6 @@ var printList = function (a) {
   }
 };
 
-// const newHead = Intersection(a, e)
-// printList(newHead)
-
-
-/* TEST */
-
-// var a = new LinkedList('a');
-// var b = new LinkedList('b');
-// var c = new LinkedList('c');
-// var d = new LinkedList('d');
-// var e = new LinkedList('e');
-// var f = new LinkedList('f');
-// var g = new LinkedList('g');
-// var h = new LinkedList('h');
-
-// a.next = b;
-// b.next = c;
-// c.next = d;
-// d.next = e;
-// e.next = f;
-// f.next = g;
-// g.next = h;
-
-// var a1 = new LinkedList('a1');
-// var b1 = new LinkedList('b1');
-// var c1 = new LinkedList('c1');
-
-// a1.next = b1;
-// b1.next = c1;
-// c1.next = d1;
-
-
-
-
-var intersectNode = intersection(a, e);
-print(intersection(a,e));
-console.log(intersection(a,e));
+var newHead = intersection(a, f);
+printList(newHead);
+// console.log(intersection(a,f));
--- chapter02/tempCodeRunnerFile.js
+++ chapter02/tempCodeRunnerFile.js
@@ -1,2 +1,25 @@
-ersection(a, e);
-printList(intersection(
+  var intersection = function(headA, headB) {
+          let currA = headA;
+          let currB = headB;
+          // console.log(currA);
+          // console.log(currB);
+          
+          while (currA !==currB) {
+              currA = currA === null ? headB : currA.next;
+              currB = currB === null ? headA : currB.next;
+              
+              if (currA === currB) {
+                return currA;
+              }
+            }
+            
+            console.log('currA:',currA);
+          return currA;
+      };
+
+       const Node = function(val) {
+           this.val = val;
+           this.next = next;
+       }
+      
+console.log(intersection(a,e));
